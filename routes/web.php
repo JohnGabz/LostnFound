8a2d5631f0d7265f8e66c5123401db4394c40614
IrideV2 <162785045+IrideV2@users.noreply.github.com>
2025-05-06 13:46:34 +0800

    routes update

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 //Default page
 Route::get('/', function () {
-    return view('login');
+    return view('authentication/login');
 });
 
 //Login and Register
 Route::get('/login', function () {
-    return view('login');
+    return view('authentication/login');
 });
 Route::get('/register', function () {
-    return view('register');
+    return view('authentication/register');
 });
